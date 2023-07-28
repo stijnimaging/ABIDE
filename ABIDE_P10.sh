b0a087dca6959cc3426e27896398e5ee8de5cbdc
Stijn <stijn.imaging@gmail.com>
2023-07-28 14:19:24 +0200

    Add files via upload

---
+++ ABIDE_P10.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# Run batch 
+# echo "Start time : $(date +"%T")" && aws
+
+# echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Pitt_0050002 Pitt_0050002 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Olin_0050108 FS_Olin_0050108 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Olin_0050137 FS_Olin_0050137 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/OHSU_0050155 FS_OHSU_0050155 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/OHSU_0050165 FS_OHSU_0050165 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Trinity_0050238 FS_Trinity_0050238 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Trinity_0050244 FS_Trinity_0050244 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050309 FS_UM_1_0050309 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050323 FS_UM_1_0050323 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050328 FS_UM_1_0050328 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050378 FS_UM_1_0050378 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_2_0050423 FS_UM_2_0050423 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050432 FS_USM_0050432 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050450 FS_USM_0050450 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050450 FS_USM_0050450 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050451 FS_USM_0050451 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050452 FS_USM_0050452 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050454 FS_USM_0050454 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050456 FS_USM_0050456 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050457 FS_USM_0050457 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050458 FS_USM_0050458 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050459 FS_USM_0050459 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050460 FS_USM_0050460 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050461 FS_USM_0050461 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050462 FS_USM_0050462 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050464 FS_USM_0050464 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050465 FS_USM_0050465 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050471 FS_USM_0050471 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050472 FS_USM_0050472 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050473 FS_USM_0050473 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050474 FS_USM_0050474 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050475 FS_USM_0050475 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050476 FS_USM_0050476 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050478 FS_USM_0050478 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050479 FS_USM_0050479 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050484 FS_USM_0050484 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050495 FS_USM_0050495 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050506 FS_USM_0050506 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050508 FS_USM_0050508 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050512 FS_USM_0050512 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050513 FS_USM_0050513 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050517 FS_USM_0050517 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/USM_0050522 FS_USM_0050522 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Leuven_2_0050734 FS_Leuven_2_0050734 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/KKI_0050805 FS_KKI_0050805 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/KKI_0050806 FS_KKI_0050806 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/KKI_0050808 FS_KKI_0050808 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/KKI_0050809 FS_KKI_0050809 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/KKI_0050810 FS_KKI_0050810 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/KKI_0050811 FS_KKI_0050811 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/KKI_0050813 FS_KKI_0050813 --recursive --no-sign-request --quiet
+
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/NYU_0050953 FS_NYU_0050953 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/NYU_0050971 FS_NYU_0050971 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/NYU_0050975 FS_NYU_0050975 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/NYU_0050980 FS_NYU_0050980 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/NYU_0051108 FS_NYU_0051108 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/NYU_0051115 FS_NYU_0051115 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/NYU_0051119 FS_NYU_0051119 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/NYU_0051120 FS_NYU_0051120 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/NYU_0051125 FS_NYU_0051125 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/STANFORD_0051176 FS_STANFORD_0051176 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UCLA_1_0051232 FS_UCLA_1_0051232 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UCLA_1_0051233 FS_UCLA_1_0051233 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UCLA_1_0051242 FS_UCLA_1_0051242 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UCLA_1_0051243 FS_UCLA_1_0051243 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UCLA_1_0051244 FS_UCLA_1_0051244 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UCLA_1_0051245 FS_UCLA_1_0051245 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UCLA_1_0051246 FS_UCLA_1_0051246 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UCLA_1_0051247 FS_UCLA_1_0051247 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UCLA_1_0051259 FS_UCLA_1_0051259 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UCLA_1_0051270 FS_UCLA_1_0051270 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UCLA_2_0051310 FS_UCLA_2_0051310 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/MAX_MUN_0051366 FS_MAX_MUN_0051366 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/MAX_MUN_0051367 FS_MAX_MUN_0051367 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/MAX_MUN_0051368 FS_MAX_MUN_0051368 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/MAX_MUN_0051371 FS_MAX_MUN_0051371 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/MAX_MUN_0051372 FS_MAX_MUN_0051372 --recursive --no-sign-request --quiet
+#echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/CALTECH_0051475 FS_CALTECH_0051475 --recursive --no-sign-request --quiet
+
+
+
---
+++ ABIDE_P2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Run batch
+
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Trinity_0050262 FS_Trinity_0050262 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Trinity_0050263 FS_Trinity_0050263 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Trinity_0050264 FS_Trinity_0050264 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Trinity_0050265 FS_Trinity_0050265 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Trinity_0050266 FS_Trinity_0050266 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Trinity_0050267 FS_Trinity_0050267 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Trinity_0050268 FS_Trinity_0050268 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Trinity_0050269 FS_Trinity_0050269 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Trinity_0050270 FS_Trinity_0050270 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/Trinity_0050271 FS_Trinity_0050271 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050272 FS_UM_1_0050272 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050273 FS_UM_1_0050273 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050274 FS_UM_1_0050274 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050275 FS_UM_1_0050275 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050276 FS_UM_1_0050276 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050277 FS_UM_1_0050277 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050278 FS_UM_1_0050278 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050279 FS_UM_1_0050279 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050280 FS_UM_1_0050280 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050281 FS_UM_1_0050281 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050282 FS_UM_1_0050282 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050283 FS_UM_1_0050283 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050284 FS_UM_1_0050284 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050285 FS_UM_1_0050285 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050286 FS_UM_1_0050286 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050287 FS_UM_1_0050287 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050288 FS_UM_1_0050288 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050289 FS_UM_1_0050289 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050290 FS_UM_1_0050290 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050291 FS_UM_1_0050291 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050292 FS_UM_1_0050292 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050293 FS_UM_1_0050293 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050294 FS_UM_1_0050294 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050295 FS_UM_1_0050295 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050296 FS_UM_1_0050296 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050297 FS_UM_1_0050297 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050298 FS_UM_1_0050298 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050299 FS_UM_1_0050299 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050300 FS_UM_1_0050300 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050301 FS_UM_1_0050301 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050302 FS_UM_1_0050302 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050303 FS_UM_1_0050303 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050304 FS_UM_1_0050304 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050305 FS_UM_1_0050305 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050306 FS_UM_1_0050306 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050307 FS_UM_1_0050307 --recursive --no-sign-request --quiet
+echo "Start time : $(date +"%T")" && aws s3 cp s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/UM_1_0050308 FS_UM_1_0050308 --recursive --no-sign-request --quiet
